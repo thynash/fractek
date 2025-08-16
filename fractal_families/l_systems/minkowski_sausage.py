e6d5b7ac67f9c92456fd277cb22b0c058c6be45f
thynash <24mc3033@rgipt.ac.in>
2025-08-16 12:06:05 +0000

    minkowski_sausage added

---
+++ fractal_families/l_systems/minkowski_sausage.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_minkowski_sausage(order=3, step=10):
+    rules = {'F': 'FLFRRFLF'}
+    seq = 'F'
+    for _ in range(order):
+        seq = ''.join(rules.get(c, c) for c in seq)
+    x, y, angle = [0], , 0
+    for move in seq:
+        if move == 'F':
+            x.append(x[-1] + step * np.cos(np.deg2rad(angle)))
+            y.append(y[-1] + step * np.sin(np.deg2rad(angle)))
+        elif move == 'L':
+            angle += 90
+        elif move == 'R':
+            angle -= 90
+    return np.array(x), np.array(y)
+
+def plot_minkowski_sausage(x, y):
+    plt.figure()
+    plt.plot(x, y, color='orange')
+    plt.axis('off')
+    plt.axis('equal')
+    plt.title("Minkowski Sausage")
+    plt.show()
+
+def info_minkowski_sausage(order):
+    return {"order": order, "curve_length": 8**order}
+
